634407bb287d477e08bd49a31d42b35dc03cb580
shivanialane <93445839+shivanialane@users.noreply.github.com>
2023-04-20 18:48:53 +0530

    Update recursiveparser.c

--- CC lab/Recursiveparse/recursiveparser.c
+++ CC lab/Recursiveparse/recursiveparser.c
@@ -11,114 +11,107 @@ void main()
 {
 printf(" enter the string\n");
 scanf("%s",input);
-    if(S()==1)
-    {
-        printf(" \n string is accepted");
-    }
-    else
-    {
-        printf("\n string is not accepted\n");
-    }
+if(S()==1){
+    printf(" \n string is accepted");
+}
+else{
+    printf("\n string is not accepted\n");
+}
 }
 int S()
 {
     if(A()==1)
-        if(B()==1)
-           if(input[i]=='a')
-           {
-               i++;
-           if(input[i]=='b')
-           {
-               i++;
-if(C()==1)
-if(D()==1)
-if(input[i]=='e')
-{
-i++;
-if(input[i]=='f')
-{
-i++;
-return 1;
-}
-else return 0;
-}
-else return 0;
-else return 0;
-else return 0;
-}
-else return 0;
-}
-else return 0;
-else return 0;
-else return 0;
+    if(B()==1)
+    if(input[i]=='a'){
+        i++;
+        if(input[i]=='b'){
+            i++;
+            if(C()==1)
+            if(D()==1)
+            if(input[i]=='e'){
+                i++;
+                if(input[i]=='f'){
+                    i++;
+                    return 1;
+                }
+                else return 0;
+            }
+            else return 0;
+            else return 0;
+            else return 0;
+        }
+        else return 0;
+    }
+    else return 0;
+    else return 0;
+    else return 0;
 }
 int A()
 {
-if(B()==1)
-if(input[i]=='b')
-{
-i++;
-if(input[i]=='c')
-{
-i++;
-return 1;
-}
-else return 0;
-}
-else return 0;
-else return 0;
+    if(B()==1)
+    if(input[i]=='b')
+    {
+        i++;
+        if(input[i]=='c')
+        {
+            i++;
+            return 1;
+        }
+        else return 0;
+    }
+    else return 0;
+    else return 0;
 }
 int B()
 {
-if(C()==1)
-if(input[i]=='b')
-{
-i++;
-if(input[i]=='c')
-{
-i++;
-if(input[i]=='d')
-{
-i++;
-return 1;
-}
-else return 0;
-}
-else return 0;
-}
-else return 0;
-else return 0;
+    if(C()==1)
+    if(input[i]=='b')
+    {
+        i++;
+        if(input[i]=='c')
+        {
+            i++;
+            if(input[i]=='d')
+            {
+                i++;
+                return 1;
+            }
+            else return 0;
+        }
+        else return 0;
+    }
+    else return 0;
+    else return 0;
 }
 int C()
 {
-if(input[i]=='c')
-{
-i++;
-if(input[i]=='d')
-{
-i++;
-if(input[i]=='e')
-{
-i++;
-if(input[i]=='f')
-{
-i++;
-return 1;
-}
-else return 0;
-}
-else return 0;
-}
-else return 0;
-}
-else return 0;
+    if(input[i]=='c')
+    {
+        i++;
+        if(input[i]=='d')
+        {
+            i++;
+            if(input[i]=='e')
+            {
+                i++;
+                if(input[i]=='f')
+                {
+                    i++;
+                    return 1;
+                }
+                else return 0;
+            }
+            else return 0;
+        }
+        else return 0;
+    }
+    else return 0;
 }
 int D()
 {
-if(input[i]=='d')
-{
-i++;
-return 1;
-}
-else return 0;
+    if(input[i]=='d'){
+        i++;
+        return 1;
+    }
+    else return 0;
 }
